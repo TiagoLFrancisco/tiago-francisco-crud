1ba031a5b6cbf80e7f056ffe429aef6f42056b27
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-03-21 18:08:31 +0000

    Renaming old columns names and adding new ones. They are now:	Firstname, Lastname, DateOfBirth,	PhoneNumber, Email, BankAccountNumber

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,23 +1,43 @@
 import { Component } from '@angular/core';
 
 export interface PeriodicElement {
-  name: string;
+  firstName: string;
   position: number;
-  weight: number;
-  symbol: string;
+  lastName: string;
+  dateOfBirth: string;
+  phoneNumber: string;
+  email: string;
+  bankAccountNumber: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  {
+    position: 1,
+    firstName: 'Andrea',
+    lastName: 'Pacoal',
+    dateOfBirth: '01/02/93',
+    phoneNumber: '912734333',
+    email: 'a.pascoal@gmail.com',
+    bankAccountNumber: 345654123,
+  },
+  {
+    position: 2,
+    firstName: 'Augusto',
+    lastName: 'Albertino',
+    dateOfBirth: '04/05/94',
+    phoneNumber: '912734111',
+    email: 'a.albertino@gmail.com',
+    bankAccountNumber: 245678129,
+  },
+  {
+    position: 3,
+    firstName: 'Matilde',
+    lastName: 'Santos',
+    dateOfBirth: '06/07/95',
+    phoneNumber: '912734222',
+    email: 'matilde.s@gmail.com',
+    bankAccountNumber: 849099125,
+  },
 ];
 
 @Component({
@@ -28,9 +48,12 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class HomeComponent {
   displayedColumns: string[] = [
     'position',
-    'name',
-    'weight',
-    'symbol',
+    'firstName',
+    'lastName',
+    'dateOfBirth',
+    'phoneNumber',
+    'email',
+    'bankAccountNumber',
     'actions',
   ];
   dataSource = ELEMENT_DATA;
